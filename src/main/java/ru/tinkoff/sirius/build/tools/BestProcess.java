e1519767b2cec5f24a84c019ff454a88f48b4091
tim <t.p.maerov@tinkoff.ru>
2022-08-05 20:02:22 +0300

    init

---
+++ src/main/java/ru/tinkoff/sirius/build/tools/BestProcess.java
@@ -0,0 +1,13 @@
+package ru.tinkoff.sirius.build.tools;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BestProcess {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BestProcess.class, args);
+    }
+
+}
