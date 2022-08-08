4aae8ec1fc42d4f978afd3acb7d6c14a1d4ec8ad
tim <t.p.maerov@tinkoff.ru>
2022-08-08 17:24:33 +0300

    simple project

--- src/main/java/ru/tinkoff/sirius/build/tools/BestProcess.java
+++ src/main/java/ru/tinkoff/sirius/build/tools/BestProcess.java
@@ -1,13 +1,9 @@
 package ru.tinkoff.sirius.build.tools;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication
 public class BestProcess {
 
     public static void main(String[] args) {
-        SpringApplication.run(BestProcess.class, args);
+        System.out.println("hello-world");
     }
 
 }
