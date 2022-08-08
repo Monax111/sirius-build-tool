7deeb324933b51258cc04f668292a3bd0edc932f
tim <t.p.maerov@tinkoff.ru>
2022-08-08 17:34:06 +0300

    use gson

--- src/main/java/ru/tinkoff/sirius/build/tools/BestProcess.java
+++ src/main/java/ru/tinkoff/sirius/build/tools/BestProcess.java
@@ -1,9 +1,13 @@
 package ru.tinkoff.sirius.build.tools;
 
+import com.google.gson.Gson;
+
 public class BestProcess {
 
     public static void main(String[] args) {
-        System.out.println("hello-world");
+        Foo foo = new Foo("hi");
+        String json = new Gson().toJson(foo);
+        System.out.println(json);
     }
 
 }
---
+++ src/main/java/ru/tinkoff/sirius/build/tools/Foo.java
@@ -0,0 +1,11 @@
+package ru.tinkoff.sirius.build.tools;
+
+public class Foo {
+
+    public String bar;
+
+    public Foo(String bar) {
+        this.bar = bar;
+    }
+
+}
