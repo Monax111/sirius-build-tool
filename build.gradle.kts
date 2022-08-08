95c24f508d4d3857ad3797fa9eba3ad3b7eb4463
tim <t.p.maerov@tinkoff.ru>
2022-08-08 17:34:06 +0300

    use gson

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,20 @@
 plugins{
     java
 }
+
+group = "ru.tinkoff.sirius.build.tools"
+version = "1.0"
+
+java{
+    toolchain{
+        languageVersion.set(JavaLanguageVersion.of(17))
+    }
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("com.google.code.gson:gson:2.9.1")
+}
